7f70da87e4356c5007514469fb1a6ed4659967a4
Yumi (Mun Suen) Loh <yumil@orsted.com>
2023-12-01 15:31:01 +0800

    add countdown

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let index = 0;
+document.addEventListener("DOMContentLoaded", getHolidays());
 
 async function getHolidays(){
     const holidays = await fetch('holidays.json', { mode: 'no-cors'})
@@ -7,19 +8,19 @@ async function getHolidays(){
                             .catch(error => console.error("Error fetching data", error));
 
     const currentDate = new Date();
-    const nextHolidays = holidays.holidays.find(holiday => new Date(holiday.date) > currentDate);
-    console.log(nextHolidays);
+    const nextHoliday = holidays.holidays.find(holiday => new Date(holiday.date) > currentDate);
     let dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
     let mainContainer = document.getElementById('nextHolidayContainer');
     mainContainer.textContent = '';
     let holidayName = document.createElement('p');
     let holidayDate = document.createElement('p');
-    holidayName.textContent = nextHolidays.name;
-    holidayDate.textContent = new Date(nextHolidays.date).toLocaleDateString('en-US', dateOptions);
+    holidayName.textContent = nextHoliday.name;
+    holidayDate.textContent = new Date(nextHoliday.date).toLocaleDateString('en-US', dateOptions);
     mainContainer.appendChild(holidayName);
     mainContainer.appendChild(holidayDate);
     index = 0;
+    countdown(nextHoliday.date);
 }
 
 async function getSubsequentHoliday(){
@@ -39,4 +40,13 @@ async function getSubsequentHoliday(){
     holidayDate.textContent = new Date(nextHolidays[index].date).toLocaleDateString('en-US', dateOptions);
     mainContainer.appendChild(holidayName);
     mainContainer.appendChild(holidayDate);
+    countdown(nextHolidays[index].date);
+}
+
+function countdown(nextHolidayDate){
+    let currentDate = new Date();
+    let nextDate = new Date(nextHolidayDate);
+    const dayDifference = Math.ceil((nextDate.getTime() - currentDate.getTime()) / (1000*60*60*24));
+    let h1 = document.getElementById('h1');
+    h1.textContent = `The next holiday is on: ${dayDifference} days later`;
 }
